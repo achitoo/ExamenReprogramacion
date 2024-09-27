613604941de71f167d76c2b9d35c6c3f294dd196
achitoo <hbaltodano@uamv.edu.ni>
2024-09-27 09:43:53 -0600

    Primer commit pd: profe losiento hasta despues lei que tenia que hacer un commit por cada clase :(

---
+++ Corte 1/Corte 1/Models/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte_1.Models
+
+{
+    internal class Registro
+    {
+        private Persona[] personas;
+        private int contador;
+        private const int MAX = 25;
+
+        public Registro()
+        {
+            personas = new Persona[MAX];
+            contador = 0;
+        }
+
+        public void AgregarPersona(Persona persona)
+        {
+            if (contador < MAX)
+            {
+                personas[contador] = persona;
+                contador++;
+            }
+            else
+            {
+                MessageBox.Show("No se pueden agregar más elementos", "Personas", MessageBoxButtons.OK, MessageBoxIcon.Error); ;
+            }
+        }
+
+        public Persona ObtenerPersona(int indice)
+        {
+            if (indice >= 0 && indice < contador)
+            {
+                return personas[indice];
+            }
+            return null;
+        }
+
+        public int Contador
+        {
+            get { return contador; }
+        }
+    }
+}
---
+++ Corte 1/Corte 1/Models/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte_1.Models
+{
+    internal class Persona
+    {
+        private string nombre {  get; set; }
+
+        private string apellido { get; set; }
+
+        private DateTime fechaNacimiento { get; set; }
+
+        private string ciudad {  get; set; }
+
+        public Persona(string nombre, string apellido, DateTime fechaNacimiento, string ciudad)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechaNacimiento = fechaNacimiento;
+            this.ciudad = ciudad;
+        }
+    }
+}
